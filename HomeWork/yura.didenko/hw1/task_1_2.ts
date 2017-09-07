063cfed26425af165c962e902e699e8b99502731
Didenko Yura <didenkot34@gmail.com>
2017-09-07 11:24:29 +0300

    Changed args in function getDate

--- HomeWork/yura.didenko/hw1/task_1_2.ts
+++ HomeWork/yura.didenko/hw1/task_1_2.ts
@@ -1,23 +1,25 @@
+'use strict';
 /**
  *Implement override method that can retrieve day, month(as number as text format) and year and return Date object
  *Override this method for retrieve array of such parameters and retrieve array with dates.
  */
-
-'use strict';
 function getDate(day:number, month:number|string, year:number):Date;
-function getDate(dateConfig: [number,string] | [number] ):Date[];
+function getDate(dateConfig:[number,string] | [number]):Date[];
 /**
  *
- * @param args
+ * @param dateConfig
+ * @param month
+ * @param year
  * @returns {Date | Date[]}
  */
-function getDate(...args):Date | Date[] {
+function getDate(dateConfig:number | [number,string] | [number], month?:number|string, year?:number):Date | Date[] {
 
-    if (!Array.isArray(args[0])) {
-        return new Date(`${args[0]}, ${args[1]}, ${args[2]}`);
+    if (Array.isArray(dateConfig)) {
+        return new Date(`${dateConfig[0]}, ${dateConfig[1]}, ${dateConfig[2]}`);
     }
 
-    return [new Date(`${args[0][0]}, ${args[0][1]}, ${args[0][2]}`)]
+    return [new Date(`${dateConfig}, ${month}, ${year}`)]
+
 }
 
 getDate(3, 'August', 15);
