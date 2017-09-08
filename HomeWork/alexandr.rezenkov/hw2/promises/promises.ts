657c227097ff32c6d38e7a4601d76a0fc736ef92
Aleksandr Rezenkov <aleksandr.rezenkov@sky-tech.co>
2017-09-08 14:40:12 +0300

    promise

---
+++ HomeWork/alexandr.rezenkov/hw2/promises/promises.ts
@@ -0,0 +1,14 @@
+﻿function Run<T>(timeout:number, data:T): Promise<T> {
+    return new Promise<T>((resolve) => setTimeout(() => { resolve(data); }, timeout * 1 * 1000));
+}
+
+function RunInParalell() {
+    const promise1 = Run<string>(2, 'promise 1');
+    const promise2 = Run<number>(4, 344);
+    const promise3 = Run<string>(5, 'promise 2');
+
+
+    Promise.all([promise1, promise2, promise3]).then( (values) => console.log('DONE!'))
+}
+
+RunInParalell();
