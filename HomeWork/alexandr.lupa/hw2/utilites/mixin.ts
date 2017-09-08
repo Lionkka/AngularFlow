25c54580cddc65cb1b673827bda136991ddba0ce
Alexandr Lupa <alexandr.lupa@gmail.com>
2017-09-08 14:16:28 +0300

    up

--- HomeWork/alexandr.lupa/hw2/utilites/mixin.ts
+++ HomeWork/alexandr.lupa/hw2/utilites/mixin.ts
@@ -1,3 +1,5 @@
+// great Mix which I saw in alexandr.rezenkov's work
+// will be implemented later, so as not infringe copyrights ;-)
 export default function Mix(...mixins: any[]) {
   return function (base) {
       mixins.forEach(mixin => {
