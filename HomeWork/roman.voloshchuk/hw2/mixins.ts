0b844029fc8993fa05dd7a833cb3971a5b884af1
roman.voloshchuk <mth39ALUX5RsAPN9>
2017-09-08 17:36:06 +0300

    homework1

---
+++ HomeWork/roman.voloshchuk/hw2/mixins.ts
@@ -0,0 +1,70 @@
+function applyMixins(derivedCtor: any, baseCtors: any[]){
+    baseCtors.forEach(baseCtor => {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+            derivedCtor.prototype[name] = baseCtor.prototype[name];
+        });
+    });
+}
+
+class Disposable {
+    public dispose():void {
+        Object.getOwnPropertyNames(this).forEach(name => {
+            this[name] = '';
+        });
+    }
+}
+class Deletable {
+    public delete():void {
+        Object.getOwnPropertyNames(this).forEach(name => {
+            this[name] = 'deleted';
+        });
+    }
+}
+class ReadOnly {
+    public protect():void {
+        Object.getOwnPropertyNames(this).forEach(name => {
+            let tmp = this[name];
+            Object.defineProperty(this, name, {
+                value: tmp,
+                writable: false
+            });
+        });
+    }
+}
+
+
+class MixinTest implements Disposable, Deletable, ReadOnly {
+    constructor(val:string) {
+        this.value = val;
+    }
+
+    public value: string = '';
+    set setValue(val:string){
+        this.value = val;
+    }
+
+    dispose: () => void;
+    delete: () => void;
+    protect: () => void;
+}
+
+applyMixins(MixinTest, [Disposable, Deletable, ReadOnly]);
+
+let test = new MixinTest("hello");
+test.dispose();
+test.delete();
+console.log(test);
+test.value = "again hello";
+console.log(test);
+test.protect();
+console.log(test);
+try {
+    test.value = 'fasdfwe';
+    console.log(test);
+    test.setValue = "ftf";
+    console.log(test);
+} catch (e) {
+    console.log(e);
+}
+
+
