82a0828bc3539a8df58f82e34b569efa1d198d92
Aleksandr Rezenkov <aleksandr.rezenkov@sky-tech.co>
2017-09-10 20:08:29 +0300

    hw3

---
+++ HomeWork/alexandr.rezenkov/hw3/src/Circle.ts
@@ -0,0 +1,47 @@
+﻿export class Circle {
+    private _radius: number;
+    private _x: number;
+    private _y: number;
+
+    constructor(x: number, y: number, radius: number) {
+        this.Radius = radius;
+        this.Y = y;
+        this.X = x;
+    }
+
+    public get Radius(): number {
+        return this._radius;
+    }
+
+    public set Radius(val: number) {
+        if (val < 0 || val > 100) {
+            throw new Error("Radius should be between 0 and 100");
+        }
+
+        this._radius = val;
+    }
+
+    public set X(val: number) {
+        this._x = val;
+    }
+
+    public get X(): number {
+        return this._x;
+    }
+
+    public set Y(val: number) {
+        this._y = val;
+    }
+
+    public get Y(): number {
+        return this._y;
+    }
+
+    public calculateSquare(): number {
+        return +(Math.PI * this._radius * this._radius).toFixed(2);
+    }
+
+    public calculateCircumference(): number {
+        return +(2 * Math.PI * this._radius).toFixed(2);
+    }
+}
---
+++ HomeWork/alexandr.rezenkov/hw3/test/circle.spec.ts
@@ -0,0 +1,64 @@
+﻿import { Circle } from '../src/Circle';
+import { } from 'jest';
+import { assert } from 'chai';
+
+test('Create circle with radius 20. Expect should create circle', () => {
+    //Arange and Act
+    const radius: number = 20;
+    const circle: Circle = new Circle(0, 0, radius);
+
+    //Assert
+    assert.equal(radius, circle.Radius, 'Actual radius is not equals 20');
+});
+
+test('Create circle with radius more than 100. Expect should throw Exception', () => {
+    //Arange and Act
+    const radius: number = 120;
+    let circle: Circle = null;
+
+    // Act
+    assert.throw(() => {
+        circle = new Circle(0, 0, radius);
+    }, 'Radius should be between 0 and 100');
+});
+
+test('Create circle with radius less than 0. Expect should throw Exception', () => {
+    //Arange and Act
+    const radius: number = -2;
+    let circle: Circle = null;
+
+    // Act
+    assert.throw(() => {
+        circle = new Circle(0, 0, radius);
+    }, 'Radius should be between 0 and 100');
+});
+
+test('Calculate circle square with radius 30. Expect square should be 2827.43', () => {
+    //Arange 
+    const radius: number = 30;
+    const expectedSquare: number = 2827.43;
+    const circle: Circle = new Circle(0, 0, radius);
+    let actualSquare: number = 0;
+
+    //Act
+    actualSquare = circle.calculateSquare();
+
+
+    //Assert
+    assert.equal(actualSquare, expectedSquare, "Wrong square calculation")
+});
+
+test('Calculate circle circumference with radius 30. Expect circumference should be 188.5', () => {
+    //Arange 
+    const radius: number = 30;
+    const expectedCircumference: number = 188.5;
+    const circle: Circle = new Circle(0, 0, radius);
+    let actualCircumference: number = 0;
+
+    //Act
+    actualCircumference = circle.calculateCircumference();
+
+
+    //Assert
+    assert.equal(actualCircumference, expectedCircumference, "Wrong circumference calculation")
+});
