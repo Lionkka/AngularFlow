645d87a52bbfaff460d7957a4349d72f13241d11
anton-kravchenko <antosha.kravchenko@gmail.com>
2017-09-15 15:09:39 +0300

    Fix asyncCountTotal function (homework 4)

--- HomeWork/anton.kravchenko/__tests__/week2_ex1.test.ts
+++ HomeWork/anton.kravchenko/__tests__/week2_ex1.test.ts
@@ -5,7 +5,7 @@ import Order from "../src/week2_ex4/order";
 
 describe("test Order class", () => {
     test("Test asyncCountTotal method. Add two items 50 and 50," +
-          "countTotal with 5%, should return 105 after 2 seconds.", (done: () => void) => {
+          "countTotal with 5%, should return 105 after 2 seconds.", () => {
         const order: Order = new Order();
         const item1: number = 50;
         const item2: number = 50;
@@ -14,18 +14,13 @@ describe("test Order class", () => {
         order.addItem(item1);
         order.addItem(item2);
 
-        const callback: (num: number) => void = (data: number): void => {
-            assert.equal(expectedResult, data, "Unexpected output");
-            done();
-        };
-
-        order.asyncCountTotal(tax, callback);
+        order.asyncCountTotal(tax).then((result) => assert.equal(result, expectedResult, "Unexpected output"));
     });
 
     test("Test asyncCountTotal method. Expect to throw.", () => {
         const order: Order = new Order();
         const tax: number = 101;
 
-        assert.throws(() => order.asyncCountTotal(tax, jest.fn()), "Invalid tax range");
+        order.asyncCountTotal(tax).catch((error) => expect(error.message).toEqual("Invalid tax range"));
     });
 });
