35340aeb0ee3ff85fecc396a733346a8810d566a
antosha <t@gmai.com>
2017-09-17 14:45:31 +0300

    Week 2 homework 3

--- HomeWork/anton.kravchenko/src/week2_ex6/counter/src/app/app.module.ts
+++ HomeWork/anton.kravchenko/src/week2_ex6/counter/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, CounterComponent
   ],
   imports: [
     BrowserModule
