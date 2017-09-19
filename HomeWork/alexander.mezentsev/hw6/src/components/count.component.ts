ad25facdee1c7d74eea1364dabdcd32de5104525
Alexander Mezentsev <oleksandr.miezientsev@gmail.com>
2017-09-19 18:29:29 +0300

    HW 6

--- HomeWork/alexander.mezentsev/hw6/src/components/count.component.ts
+++ HomeWork/alexander.mezentsev/hw6/src/components/count.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-count',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['../content/styles/count.component.css']
 })
 export class CountComponent {
-  title = 'count!';
+  @Input()
+  public count: number;
+  @Input()
+  public countColor: string;
 }
---
+++ HomeWork/alexander.mezentsev/hw6/src/components/counter.component.ts
@@ -0,0 +1,58 @@
+/*
+## Homework 6 ##
+ - Create counter component.
+Second substract 1 from general count. Count should be shown pretty. Html should be in separate file.
+We need to set counter step.
+If I do not push the button after each 20 seconds counter increase by 1 point and if it increase automatically counter should be colored by red.
+If I increase/decrease by button counter should be colored blue(pretty blue). Second component should contains number only.
+* */
+
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: '../content/views/counter.component.html',
+  styleUrls: ['../content/styles/counter.component.css']
+})
+export class CounterComponent implements OnInit {
+  private _timeout: number = 5; // 20 seconds
+  private timer: NodeJS.Timer;
+  public delta: string = '1';
+  public count: number = 0;
+  public countColor: string = 'blue';
+
+  public ngOnInit(): void {
+    this.startTimer();
+  }
+
+  private startTimer(): void {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      this.setCountAutomatically(true);
+    }, this._timeout * 1000);
+  }
+
+  private setCount(direction: boolean, delta: number): void {
+    if (direction) {
+      this.count += delta;
+    } else {
+      this.count -= delta;
+    }
+    this.startTimer();
+  }
+
+  public setCountManually(direction: boolean): void {
+    const delta: number = Number(this.delta);
+    if (isNaN(delta)) {
+      return;
+    }
+    this.countColor = 'blue';
+    this.setCount(direction, delta);
+  }
+
+  public setCountAutomatically(direction: boolean): void {
+    this.countColor = 'red';
+    const delta: number = Number(this.delta);
+    this.setCount(direction, isNaN(delta) ? 1 : delta);
+  }
+}
