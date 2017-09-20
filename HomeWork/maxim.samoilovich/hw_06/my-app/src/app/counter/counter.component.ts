cf2b3a8eee4249de52e8c0fc82ad70c63fef9e3b
Sergey Zotenko <sergey.zotenko@dev-pro.net>
2017-09-20 11:52:50 +0300

    Added comments to homeworks.

--- HomeWork/maxim.samoilovich/hw_06/my-app/src/app/counter/counter.component.ts
+++ HomeWork/maxim.samoilovich/hw_06/my-app/src/app/counter/counter.component.ts
@@ -18,12 +18,14 @@ export class CounterComponent {
         this.manageTimer(false);
     }
 
+    // SZ methods should have access modifier
     incCounter(val: number): void {
         this.counter += val;
         clearInterval(this.timer);
         this.manageTimer();
     }
 
+    // SZ missed access modifier and return type
     decCounter(val: number) {
         this.counter -= val;
         clearInterval(this.timer);
@@ -31,14 +33,22 @@ export class CounterComponent {
     }
 
     startTimer() {
+        // SZ you won't need this if you are use arrow function
+        // SZ arrow function doesn't has own context, it use context of scope
+        // where it was declared
         const that = this;
+
+        // SZ please use arrow function
         return new Promise(function(resolve, reject) {
             that.timer = setInterval(() => {
+                // SZ there is no sence to resolve a promise more than onece
                 resolve();
             }, 20000);
         });
     }
 
+    // SZ parameters should have type too
+    // SZ auto: boolean = true
     manageTimer(auto = true) {
         if (auto) {
             this.allIsOk();
