fe217cf9bba8160b0e11bc659f443a41ed30ab63
Sergey Zotenko <sergey.zotenko@dev-pro.net>
2017-09-20 11:16:51 +0300

    Added comments to homework.

--- HomeWork/anton.kravchenko/src/week2_ex6/counter/src/app/app.component.ts
+++ HomeWork/anton.kravchenko/src/week2_ex6/counter/src/app/app.component.ts
@@ -6,16 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  // SZ all properties should have access modifier and type
   generalCount = 0;
   counter = 0;
   counterState = 'blue';
   timer;
 
+  // SZ Please use tslint file
+  // https://github.com/NickolayLototskiyDevPro/AngularFlow/blob/master/tslint.json
+  // SZ all methods shold have access modifier
   ngOnInit() {
     this.initTimer();
   }
 
   initTimer(): void {
+    // SZ you implemented OnInit interface, but didn't implement OnDestroy
+    // SZ if you didn't learn it ignore this comment
     this.timer = setTimeout(() => {
       this.counterState = 'red';
       this.counter += 1;
