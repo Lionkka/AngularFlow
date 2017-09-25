7a56b5fe9dc4b90473bf8a08b3e773b2957b16d6
Sergey Zotenko <sergey.zotenko@dev-pro.net>
2017-09-25 12:21:55 +0300

    Added comments to homework.

--- HomeWork/alexander.mezentsev/hw8/src/app/translations/EN-RU.ts
+++ HomeWork/alexander.mezentsev/hw8/src/app/translations/EN-RU.ts
@@ -1,5 +1,6 @@
 import {TranslationDictionary} from '../interfaces/translationDictionary';
 
+// SZ you are able use Map<string, string> instead. NOT for rework.
 export const EN_RU_Dictionary: TranslationDictionary = {
   'EN-RU': {
     'cherries': 'вишни',
