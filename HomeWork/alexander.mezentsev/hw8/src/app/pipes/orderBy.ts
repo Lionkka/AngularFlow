a11838da2a5d1b5a2da2722609439d1a0abdd2e2
Sergey Zotenko <sergey.zotenko@dev-pro.net>
2017-09-26 12:52:04 +0300

    Added comments to homework.

--- HomeWork/alexander.mezentsev/hw8/src/app/pipes/orderBy.ts
+++ HomeWork/alexander.mezentsev/hw8/src/app/pipes/orderBy.ts
@@ -7,10 +7,14 @@ export class OrderBy implements PipeTransform {
   public transform(arr: string[] | number[], desc?: boolean): string[] | number[] {
    const result: string[] | number[] = arr.slice();
     if (typeof result[0] === 'number') {
+      // SZ (<number[]>result) is deprecated,
+      // SZ please use this (result as number[]).sort(...);
+      // SZ NOT for rework.
       (<number[]>result).sort((a: number, b: number) => {
         return a - b;
       });
     } else {
+      
       (<string[]>result).sort();
     }
     if (desc) {
